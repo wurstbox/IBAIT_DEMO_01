f0a8ef21cb2f12e4aa4ed9692c63d22bfb2ea754
wurstbox <pet@wurstbox.com>
2021-02-18 06:34:41 +0100

    Correction: Hello World => Hello, World

--- src/main/java/ibait_demo_01/Main.java
+++ src/main/java/ibait_demo_01/Main.java
@@ -1,11 +1,15 @@
 package ibait_demo_01;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		//correction: Hello World requires a comma: "Hello, World"
+		System.out.println("Hello, World");
 		System.out.println("This is Feature 1");
 	}
 
