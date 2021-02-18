8c30f337eb00d0a93578b2ffee43c0636d372b12
wurstbox <pet@wurstbox.com>
2021-02-18 06:43:43 +0100

    Extracting methods

--- src/main/java/ibait_demo_01/Main.java
+++ src/main/java/ibait_demo_01/Main.java
@@ -5,8 +5,18 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		sayHello();
+		runFeature1();
+	}
+
+	private static void runFeature1()
+	{
 		System.out.println("This is Feature 1");
+	}
+
+	private static void sayHello()
+	{
+		System.out.println("Hello World");
 	}
 
 }
